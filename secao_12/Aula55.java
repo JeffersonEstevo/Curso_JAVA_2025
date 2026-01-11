6101bf7505a5b24b213a4c71737d684009eb6baa
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-01-11 11:53:36 -0300

    enviando aula 55

---
+++ secao_12/Aula55.java
@@ -0,0 +1,21 @@
+// Operadores de Atribuição
+
+/* 
+OPERADORES DE ATRIBUIÇÃO
+
+Os operadores de atribuição são usados para atribuir valores a variáveis:
+    • Atribuição (=)
+    • Atribuição de adição (+=)
+    • Atribuição de subtração (-=)
+    • Atribuição de multiplicação (*=)
+    • Atribuição de divisão (/=)
+    • Atribuição de módulo (%=)
+*/
+
+public class Aula55 {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 2; // Equivalente a -> a = a + 2
+        System.out.println(a); // Saída: 12
+    }
+}
