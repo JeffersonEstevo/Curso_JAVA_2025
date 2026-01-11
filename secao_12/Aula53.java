b610c3b777b9278e566224df9de848c18c9bf986
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-01-11 11:42:15 -0300

    enviando aula 53

---
+++ secao_12/Aula53.java
@@ -0,0 +1,29 @@
+// Operadores de Comparação
+
+/* 
+OPERADORES DE COMPARAÇÃO
+
+Os operadores aritméticos são usados para comparar dois valores:
+    • Igual a (==)
+    • Diferente de  (!=)
+    • Maior que (>)
+    • Menor que (<)
+    • Maior ou igual a (>=)
+    • Menor ou igual a (<=)
+
+*/
+
+public class Aula53 {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 20;
+
+       System.out.println(a == b); // Saída: false
+       System.out.println(a != b); // Saída: true
+       System.out.println(a > b); // Saída: false
+       System.out.println(a < b); // Saída: true
+       System.out.println(a >= b); // Saída: false
+       System.out.println(a <= b); // Saída: true
+
+    }
+}
