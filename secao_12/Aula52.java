15aec717bfdf7844ed558fb4635ac50ea90349b9
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-01-11 11:37:19 -0300

    enviando aula 52

---
+++ secao_12/Aula52.java
@@ -0,0 +1,34 @@
+// Operadores aritméticos
+
+/* Tipos de operadores
+Em Java, os operadores são divididos em vários grupos:
+    • Operadores Aritméticos: Realizam operações matemáticas.
+    • Operadores de Comparação: Comparam dois valores.
+    • Operadores Lógicos: Combinam duas ou mais condições.
+    • Operadores de Atrbuição: Atribuem valores a variáveis.
+    • Operadores de Incremento e Decremento: Manipulação curtas de valores.
+
+OPERADORES ARITMÉTICOS
+
+Os operadores aritméticos são usados para relaizar operações matemáticas comuns:
+    • Adição (+)
+    • Subtração (-)
+    • Multiplicação (*)
+    • Divisão (/)
+    • Módulo (resto da divisão) (/)
+
+*/
+
+public class Aula52 {
+    public static void main(String[] args) {
+       int a = 10;
+       int b = 2;
+
+       System.out.println(a + b); // Saída: 12
+       System.out.println(a - b); // Saída: 8
+       System.out.println(a * b); // Saída: 20
+       System.out.println(a / b); // Saída: 5
+       System.out.println(a % b); // Saída: 0
+
+    }
+}
