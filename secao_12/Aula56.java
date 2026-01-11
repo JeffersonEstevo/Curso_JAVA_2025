0727a0484b45fe76db30f4efa16c2e23c3f562fb
JeffersonEstevo <jefferson.estevo@gmail.com>
2026-01-11 12:01:05 -0300

    enviando aula 56

---
+++ secao_12/Aula56.java
@@ -0,0 +1,32 @@
+// Operadores Unários
+
+/* 
+OPERADORES UNÁRIOS
+
+Os operadores unários são usados para operar em um único operando:
+    • Incremento (++)
+    • Decremento (--)
+    • Positivo (+)
+    • Negatico (-)
+    • Negação lógica (!)
+*/
+
+public class Aula56 {
+    public static void main(String[] args) {
+        int a = 10;
+        a++; // Equivalente a -> a = a + 1
+        System.out.println(a); // Saída: 11
+
+        /*
+        int a = 5;
+
+        // Pré-incremento int b = ++a; // Agora, a é 6 e b também é 6
+
+        // Pós-incremento int c = a++; // Agora, a é 7, mas c é 6 (porque o valor antigo de a foi usado)
+
+        // Pré-decremento int d = --a; Agora, a é 6 e d tmabém é 6
+
+        // Pós-decremnto itn e = a--; Agora, a é 5, mas e é 6 (porque o vlaor antigo de a foi usado)
+        */
+    }
+}
